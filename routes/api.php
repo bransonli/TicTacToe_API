8a0172ba5bd8efbcda28af296a717ea312a0bbb5
aaron li <bransonchen1014@gmail.com>
2021-09-03 21:06:09 +0800

    Checking game status function

--- routes/api.php
+++ routes/api.php
@@ -43,6 +43,74 @@ Route::post('/games', function(){
 Route::post('/games/{id}', function($id){
     $player = request()->input('player');
     $move = request()->input('move');
+    $board //board in 2d array form 
+
+    //processing input 
+    $move = str_split($move);
+    $col = (int)$move[0];
+    $row = (int)$move[1];
+
+    //Make a move 
+    $board[$col][$row] = (int)$player
+
+
+    //checking results 
+
+    //horiztonal 
+    for ($i =0; $i<3; $i+=1){
+        $counter = 0; 
+        $reference = $board[$i][1];
+
+            for ($j = 0; $j<3; $j+=1){
+                if ($board[$i][$j] != $reference) {
+                    $reference = $board[$i][$j];
+                } else {
+                    $counter = $counter + 1;
+                }
+            }
+
+            if ($counter == 3) {
+                return $reference;
+            }
+    }
+
+    //vertical 
+    for ($i =0; $i<3; $i+=1){
+        $counter = 0;
+        $reference = $board[1][$i];
+        for ($j = 0; $j<3; $j+=1){
+            if ($board[$j][$i] != $reference){
+                $reference = $board[$j][$i];
+
+            } else {
+                $counter = $counter + 1;
+            }
+
+        if ($counter == 3){
+            return $reference;
+        }
+
+    }
+
+    //diagonal 
+
+    if (($board[0][0] == 1 and $board[1][1] == 1 and $board[2][2] == 1) or ($board[0][2] == 1 and $board[1][1] == 1 and $board[2][0] == 1 )){
+        return 1;
+    } elseif (($board[0][0] == 2 and $board[1][1] == 2 and $board[2][2] == 2) or ($board[0][2] == 2 and $board[1][1] == 2 and $board[2][0] == 2 )){
+        return 2;
+    }
+
+    //still in progress
+    for ($i =0; $i<3; $i+=1){
+        for ($j = 0; $j<3; $j+=1){
+            if ($board[$i][$j] == 0){
+                return 0;
+            }
+        } 
+    }
+
+
+    return -1;
 
 
 });
